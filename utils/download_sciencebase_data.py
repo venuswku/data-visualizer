016ebf5519c498b515319374824e6a020ff22822
Venus Ku <venuswku@gmail.com>
2023-01-31 22:18:36 -0800

    saving first draft of script for preprocessing downloaded data

--- utils/download_sciencebase_data.py
+++ utils/download_sciencebase_data.py
@@ -1,6 +1,6 @@
-# cd C:\Users\Venuxk\Projects\data-visualizer\utils
+# cd C:\Users\Venuxk\Projects\data-visualizer
 # conda activate visualizer
-# python download_sciencebase_data.py
+# python ./utils/download_sciencebase_data.py
 
 # Standard library imports
 import os
@@ -9,74 +9,57 @@ import json
 # External dependencies imports
 import requests
 from sciencebasepy import SbSession
-import re
 
 # -------------------------------------------------- Global Variables --------------------------------------------------
 sb = SbSession()
 id_to_title = {}
 
 # -------------------------------------------------- Helper Methods --------------------------------------------------
-def download_children(item_id: str, parent_dir_path: str, item_level: int = 1) -> None:
+def download_children(item_id: str, parent_dir_path: str) -> None:
     """
     Recursively downloads all children of the item with the given ID.
 
     Args:
         item_id (str): ID of the item that potentially contains child items to download
         parent_dir_path (str): Location of the directory where child items are downloaded to
-        item_level (int): Tree level of the given item
-            ^ used to prevent creating nested subdirectories (not compatible with DataMap) within the root data directory
     """
+    global sb, id_to_title
     item_json = sb.get_item(item_id)
     item_title = item_json["title"]
+    # Create a directory to store the given item's own children/attached items.
+    id_to_title[item_id] = item_title
+    item_dir_path = os.path.join(parent_dir_path, item_id)
+    if not os.path.exists(item_dir_path): os.makedirs(item_dir_path)
     if item_json["hasChildren"]:
         # print("Looking in {} for data files...".format(item_title))
-        # Create a directory to store the given item's own children items.
-        if item_level < 3:
-            global id_to_title
-            id_to_title[item_id] = item_title
-            # item_dir_path = re.sub("[ -,]", "_", os.path.join(parent_dir_path, item_title))
-            item_dir_path = os.path.join(parent_dir_path, item_id)#item_title
-            if not os.path.exists(item_dir_path):
-                os.makedirs(item_dir_path)
-                print("making dir", item_dir_path)
-        else:
-            item_dir_path = parent_dir_path
-        # Also look through each child's own child items for raw data files.
+        # Look through each child's own child items for raw data files.
         children_ids = sb.get_child_ids(item_id)
-        child_item_level = item_level + 1
         for child_id in children_ids:
-            download_children(item_id = child_id, parent_dir_path = item_dir_path, item_level = child_item_level)
+            download_children(item_id = child_id, parent_dir_path = item_dir_path)
     else:
         # print("Downloading data from {}...".format(item_title))
-        # # Create a directory to store the given item's own children items.
-        # print("dir", item_dir_path, os.path.isdir(item_dir_path))
-        # if not os.path.exists(item_dir_path):
-        #     os.makedirs(item_dir_path)
-        #     print("making dir", item_dir_path)
-        # # Get the path to the subdirectory, which will contain all the data files.
-        # [data_item_dir_path, _] = os.path.split(parent_dir_path)
-        # print(os.path.isdir(data_item_dir_path), os.path.exists(parent_dir_path))
         # Download raw data files that are attached to the current item.
-        sb.get_item_files(item_json, destination = parent_dir_path, progress_bar = True)
+        sb.get_item_files(item_json, destination = item_dir_path, progress_bar = True)
 
 # -------------------------------------------------- Main Program --------------------------------------------------
-# 1. Ask the user for the item ID of the data that they want to download.
-root_item_id = input("Enter the data's item ID: ")
-# 2. Check if the item ID exists in ScienceBase.
-print("Checking if {} is a valid item ID...".format(root_item_id))
-item_exists = requests.Session().get("https://www.sciencebase.gov/catalog/item/" + root_item_id)
-# 3. Show the download progress.
-parent_data_dir_path = os.path.abspath(".")
-root_item_dir_path = os.path.join(parent_data_dir_path, root_item_id)
-if item_exists:
-    # Download raw data files for the inputted item ID.
-    print("Inputted item ID exists in ScienceBase: Starting download...")
-    download_children(item_id = root_item_id, parent_dir_path = parent_data_dir_path)
-    # Save dictionary which maps each item's ID to their title as a JSON.
-    # ^ File paths with longer than 256 characters causes FileNotFoundErrors in Windows (https://github.com/python/cpython/issues/89935).
-    with open(os.path.join(root_item_dir_path, "sciencebase_id_to_title.json"), "w") as json_file:
-        json.dump(id_to_title, json_file, indent = 4)
-    print("Download complete! All data files are saved in {}.".format(root_item_dir_path))
-else:
-    # Display an error message if the item ID doesn't exist in ScienceBase.
-    print("Error: Inputted item ID does not exist in ScienceBase. Please run this script again with an existing item ID.")
+if __name__ == "__main__":
+    # 1. Ask the user for the item ID of the data that they want to download.
+    root_item_id = input("Please enter the data's item ID: ")
+    # 2. Check if the item ID exists in ScienceBase.
+    print("Checking if {} is a valid item ID...".format(root_item_id))
+    item_exists = requests.Session().get("https://www.sciencebase.gov/catalog/item/" + root_item_id)
+    # 3. Show the download progress.
+    parent_data_dir_path = os.path.abspath("./utils")
+    root_item_dir_path = os.path.join(parent_data_dir_path, root_item_id)
+    if item_exists:
+        # Download raw data files for the inputted item ID.
+        print("Inputted item ID exists in ScienceBase: Starting download...")
+        download_children(item_id = root_item_id, parent_dir_path = parent_data_dir_path)
+        # Save dictionary which maps each item's ID to their title as a JSON.
+        # ^ File paths with longer than 256 characters causes FileNotFoundErrors in Windows (https://github.com/python/cpython/issues/89935).
+        with open(os.path.join(root_item_dir_path, "sciencebase_id_to_title.json"), "w") as json_file:
+            json.dump(id_to_title, json_file, indent = 4)
+        print("Download complete! All data files are saved in {}.".format(root_item_dir_path))
+    else:
+        # Display an error message if the item ID doesn't exist in ScienceBase.
+        print("Error: Inputted item ID does not exist in ScienceBase. Please run this script again with an existing item ID.")
--- utils/preprocess_data.py
+++ utils/preprocess_data.py
@@ -1,6 +1,11 @@
+# cd C:\Users\Venuxk\Projects\data-visualizer
+# conda activate visualizer
+# python ./utils/preprocess_data.py
+
 # Standard library imports
 import os
-import xml.dom.minidom
+import json
+import xml.etree.ElementTree as ET
 
 # External dependencies imports
 import geopandas as gpd
@@ -9,8 +14,90 @@ import cartopy.crs as ccrs
 import rioxarray as rxr
 
 # -------------------------------------------------- Global Variables --------------------------------------------------
+dataset_crs = None
+sciencebase_download_outputted_json = "sciencebase_id_to_title.json"
 
 # -------------------------------------------------- Helper Methods --------------------------------------------------
+def get_crs_from_xml_file(file_path: str) -> ccrs:
+    """
+    Read a data file's XML (extensible markup language) to get its CRS (coordinate reference system).
+
+    Args:
+        file_path (str): Path to a data file, which is in the same directory as its XML file
+            ^ XML file contains information about the data's CRS
+    """
+    global dataset_crs
+    if dataset_crs is None:
+        # Get the path to the XML file.
+        file_dir_path, _ = os.path.split(file_path)
+        xml_file, *_ = [file for file in os.listdir(file_dir_path) if file.endswith(".xml")]
+        xml_path = os.path.join(file_dir_path, xml_file)
+        # Get proj4 parameters that correspond to metadata provided in the XML file.
+        proj4_params = {
+            # "proj": "lcc",
+            # "lat_1": 47.5,
+            # "lat_2": 48.73333333333333,
+            # "lon_0": -120.8333333333333,
+            # "lat_0": 47.0,
+            # "x_0": 500000.0,
+            # "y_0": 0.0,
+            # "units": "m",
+            # "datum": "NAD83",
+            # "ellps": "GRS80",
+            "no_defs": True,
+            "type": "crs"
+        }
+        tree = ET.parse(xml_path)
+        root = tree.getroot()
+        for projection_element in root.iter("mapprojn"):
+            proj_name = projection_element.text
+            if proj_name in ["Lambert Conformal Conic"]: proj4_params["proj"] = "lcc"
+            else: print("Error getting the CRS from {}: Script did not account for the {} projection yet.".format(xml_path, proj_name))
+        for grid_coordinate_system_element in root.iter("gridsysn"):
+            proj_name = grid_coordinate_system_element.text
+            if proj_name in ["Universal Transverse Mercator"]:
+                proj4_params["proj"] = "utm"
+                for utm_zone_element in root.iter("utmzone"): proj4_params["zone"] = utm_zone_element.text
+            else:
+                print("Error getting the CRS from {}: Script did not account for the {} projection yet.".format(xml_path, proj_name))
+        for i, standard_parallel_element in enumerate(root.iter("stdparll")):
+            if i < 2:
+                standard_parallel_param = "lat_{}".format(i + 1)
+                proj4_params[standard_parallel_param] = standard_parallel_element.text
+            else:
+                print("Error getting the CRS from {}: More than 2 standard parallels were provided in the XML file.".format(xml_path))
+        for scaling_factor_at_central_meridian_element in root.iter("sfctrmer"): proj4_params["k_0"] = scaling_factor_at_central_meridian_element.text
+        for longitude_of_central_meridian_element in root.iter("longcm"): proj4_params["lon_0"] = longitude_of_central_meridian_element.text
+        for latitude_of_projection_origin_element in root.iter("latprjo"): proj4_params["lat_0"] = latitude_of_projection_origin_element.text
+        for false_easting_element in root.iter("feast"): proj4_params["x_0"] = false_easting_element.text
+        for false_northing_element in root.iter("fnorth"): proj4_params["y_0"] = false_northing_element.text
+        for planar_distance_units_element in root.iter("plandu"):
+            units = planar_distance_units_element.text.lower()
+            if units in ["meter", "meters"]: proj4_params["units"] = "m"
+            else: print("Error getting the CRS from {}: Script did not account for the {} unit yet.".format(xml_path, planar_distance_units_element.text))
+        for horizontal_datum_element in root.iter("horizdn"):
+            datum_name = horizontal_datum_element.text
+            if datum_name in ["NAD83 (CORS96)", "D_North_American_1983"]: proj4_params["datum"] = "NAD83"
+            elif datum_name in ["WGS1984"]: proj4_params["datum"], proj4_params["proj"] = "WGS84", "longlat"
+            else: print("Error getting the CRS from {}: Script did not account for the {} datum yet.".format(xml_path, datum_name))
+        for ellipsoid_element in root.iter("ellips"):
+            ellips_name = ellipsoid_element.text
+            if ellips_name in ["GRS_1980"]: proj4_params["ellps"] = "GRS80"
+            elif ellips_name in ["WGS1984"]: proj4_params["ellps"] = "WGS84"
+            else: print("Error getting the CRS from {}: Script did not account for the {} ellipsoid yet.".format(xml_path, ellips_name))
+        # Create a CRS with the proj4 parameters (https://scitools.org.uk/cartopy/docs/latest/reference/generated/cartopy.crs.CRS.html#cartopy.crs.CRS.__init__).
+        xml_crs = ccrs.CRS(proj4_params)
+        xml_epsg_code = xml_crs.to_epsg()
+        # Return the created CRS.
+        if xml_epsg_code is not None:
+            dataset_crs = ccrs.epsg(xml_epsg_code)
+            return dataset_crs
+        else:
+            return xml_crs
+    else:
+        # Datasets should have the same CRS for all their data files, so return the found CRS if it was already previously computed.
+        return dataset_crs
+
 def convert_csv_txt_data_into_geojson(file_path: str, geojson_path: str) -> None:
     """
     Creates and saves a GeoJSON file containing Points for each data point in the given dataframe.
@@ -20,8 +107,8 @@ def convert_csv_txt_data_into_geojson(file_path: str, geojson_path: str) -> None
         geojson_path (str): Path to the newly created GeoJSON file, which is a FeatureCollection of Points
     """
     if not os.path.exists(geojson_path):
-        # Read the data file as a DataFrame and replace any NaN values with "N/A".
-        dataframe = pd.read_csv(file_path).fillna("N/A")
+        # Read the data file as a DataFrame, drop any rows with all NaN values, and replace any NaN values with "N/A".
+        dataframe = pd.read_csv(file_path).dropna(axis = 0, how = "all").fillna("N/A")
         # Ignore any unnamed columns.
         dataframe = dataframe.loc[:, ~dataframe.columns.str.match("Unnamed")]
         # Get the latitude and longitude column names.
@@ -48,28 +135,11 @@ def convert_ascii_grid_data_into_geotiff(file_path: str, geotiff_path: str) -> N
         geotiff_path (str): Path to the newly created GeoTIFF file
     """
     if not os.path.exists(geotiff_path):
-        # Read the data file's XML (extensible markup language) to get its CRS (coordinate reference system).
-        xml_file = xml.dom.minidom.parse("")
-        column_element = xml_file.getElementsByTagName("planar")
-        column_names = [element.firstChild.data for element in column_elements]
-        # https://scitools.org.uk/cartopy/docs/latest/reference/generated/cartopy.crs.CRS.html#cartopy.crs.CRS.__init__
-        geotiff_crs = ccrs.CRS({
-            "proj": "lcc",
-            "lat_1": 47.5,
-            "lat_2": 48.73333333333333,
-            "lon_0": -120.8333333333333,
-            "lat_0": 47.0,
-            "x_0": 500000.0,
-            "y_0": 0.0,
-            "units": "m",
-            "datum": "NAD83",
-            "ellps": "GRS80",
-            "no_defs": True,
-            "type": "crs"
-        })
-        # Add projection to the data file based on the XML's metadata.
+        # Get the CRS of the data file using its XML file.
+        file_crs = get_crs_from_xml_file(file_path)
+        # Add found CRS to the data file.
         dataset = rxr.open_rasterio(file_path)
-        dataset.rio.write_crs(self._epsg, inplace = True)
+        dataset.rio.write_crs(file_crs, inplace = True)
         # Save the data as a GeoTIFF.
         dataset.rio.to_raster(
             raster_path = geotiff_path,
@@ -79,7 +149,7 @@ def convert_ascii_grid_data_into_geotiff(file_path: str, geotiff_path: str) -> N
 def convert_transect_data_into_geojson(self, file_path: str, geojson_path: str) -> None:
     """
     Creates and saves a GeoJSON file containing LineStrings for each transect in the given transect file.
-    Note this method was specifically written to read transect files that are in the same format as ew_lines.txt.
+    Note: This method was specifically written to read transect files that are in the same format as ew_lines.txt.
 
     Args:
         file_path (str): Path to the file containing transect data
@@ -122,15 +192,83 @@ def convert_transect_data_into_geojson(self, file_path: str, geojson_path: str) 
         geodataframe = gpd.GeoDataFrame.from_features(
             {"type": "FeatureCollection", "features": features_list},
             crs = self._epsg
-        )#.to_crs(crs = self._default_crs)
+        )
         # Save the GeoJSON file to skip converting the data file again.
         geodataframe.to_file(geojson_path, driver = "GeoJSON")
 
+def preprocess_data(src_dir_path: str, dest_dir_path: str, dir_level: int = 1) -> None:
+    """
+    Recursively preprocess all the data in the given data directory.
+
+    Args:
+        src_dir_path (str): Location of the source directory containing data files to convert into formats that are compatible with DataMap
+        dest_dir_path (str): Location of the destination directory where newly created subdirectories or processed data files are saved
+        dir_level (int): Tree level of the given source data directory
+            ^ used to prevent creating nested subdirectories (not compatible with DataMap) within the root destination directory
+            ^ root destination directory is the only directory that contains subdirectories, each holding processed data files
+    """
+    # Create a new directory in the destination directory if it's still compatible with DataMap after it gets added.
+    src_dir_name = os.path.basename(src_dir_path)
+    new_dest_dir_path = os.path.join(dest_dir_path, src_dir_name)
+    if dir_level < 3:
+        if not os.path.exists(new_dest_dir_path): os.makedirs(new_dest_dir_path)
+    else:
+        new_dest_dir_path = dest_dir_path
+    # Look through source directory for raw data files.
+    subdir_level = dir_level + 1
+    for file in os.listdir(src_dir_path):
+        file_path = os.path.join(src_dir_path, file)
+        # Look through subdirectories for raw data files as well.
+        if os.path.isdir(file_path):
+            preprocess_data(src_dir_path = file_path, dest_dir_path = new_dest_dir_path, dir_level = subdir_level)
+        elif file != sciencebase_download_outputted_json:
+            print("Converting data files from {} into {}...".format(file_path, new_dest_dir_path))
+            name, extension = os.path.splitext(file)
+            file_format = extension.lower()
+            # Convert data file into a format that is more compatible for DataMap.
+            if file_format in [".csv", ".txt"]:
+                geojson_file = name + ".geojson"
+                print("\t{} -> {}".format(file, geojson_file))
+                convert_csv_txt_data_into_geojson(file_path, os.path.join(new_dest_dir_path, geojson_file))
+            elif file_format == ".asc":
+                geotiff_file = name + ".tif"
+                print("\t{} -> {}".format(file, geotiff_file))
+                convert_ascii_grid_data_into_geotiff(file_path, os.path.join(new_dest_dir_path, geotiff_file))
+            elif file_format not in [".xml", ".png"]:
+                print("Error converting {}: Data files with the {} file format are not supported yet.".format(file_path, file_format))
+
 # -------------------------------------------------- Main Program --------------------------------------------------
-# 1. Get the path to the data directory that the user wants to preprocess.
+if __name__ == "__main__":
+    # get_crs_from_xml_file("utils/5a01f6d0e4b0531197b72cfe/5c9bec93e4b0b8a7f62c3276/ew17_july_dem.xml")
+    # convert_csv_txt_data_into_geojson("utils/5a01f6d0e4b0531197b72cfe/57f524b9e4b0bc0bec04ee64/57f7fd11e4b0bc0bec0a1be5/ew16_feb_grainsize.csv", "data/5a01f6d0e4b0531197b72cfe/57f524b9e4b0bc0bec04ee64/ew16_feb_grainsize.geojson")
+    parent_data_dir_path = os.path.abspath("./utils")
+    unprocessed_data_dirs = [file for file in os.listdir(parent_data_dir_path) if os.path.isdir(os.path.join(parent_data_dir_path, file))]
+    num_unprocessed_data_dirs = len(unprocessed_data_dirs)
+    if num_unprocessed_data_dirs > 0:
+        # 1. Get the path to the data directory that the user wants to preprocess.
+        print("Data directories to preprocess:")
+        for i, data_dir in enumerate(unprocessed_data_dirs): print("\t[{}] {}".format(i + 1, data_dir))
+        dir_index = input("Please enter your numeric choice: ")
+        # 2. Check if the user inputted a valid choice.
+        if dir_index.isnumeric() and (0 < int(dir_index) <= num_unprocessed_data_dirs):
+            # 3. Iterate through data directories and convert data files into formats that are compatible with DataMap.
+            selected_data_dir = unprocessed_data_dirs[int(dir_index) - 1]
+            data_dir_path = os.path.join(parent_data_dir_path, selected_data_dir)
+            print("All data from {} will be preprocessed momentarily...".format(data_dir_path))
+            root_output_dir_path = os.path.abspath("./data")
+            preprocess_data(src_dir_path = data_dir_path, dest_dir_path = root_output_dir_path)
+            # 4. Rename directories if they're outputted from download_sciencebase_data.py.
+            sciencebase_download_outputted_json_file_path = os.path.join(data_dir_path, sciencebase_download_outputted_json)
+            if os.path.exists(sciencebase_download_outputted_json_file_path):
+                # Open the JSON file that maps ScienceBase item IDs to their titles.
+                json_file = open(sciencebase_download_outputted_json_file_path)
+                item_id_to_title = json.load(json_file)
+                # Replace each item ID directory with the item's title instead.
 
-# 2. Iterate through data directories and convert data files into formats that are compatible with DataMap.
-
-# 3. 
-# Rename directories if they're outputted from download_sciencebase_data.py.
-if os.path.exists(path):
+                # Get the title of the root item that contained all the downloaded items.
+                selected_data_dir = item_id_to_title[selected_data_dir]
+            print("Converting data complete! All preprocessed data files are saved in {}.".format(os.path.join(root_output_dir_path, selected_data_dir)))
+        else:
+            print("Invalid choice: Your choice {} did not match any of the ones provided above. Please run this script again with a valid numeric choice.".format(dir_index))
+    else:
+        print("Data not found: There are no data directories to preprocess. Make sure your data is placed in {}.".format(parent_data_dir_path))
